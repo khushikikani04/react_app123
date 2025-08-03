9f82ddf9c9c66df9e339e66277db4257aed08d6e
khushikikani04 <152688579+khushikikani04@users.noreply.github.com>
2025-08-03 15:59:41 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import { useEffect, useState } from "react";
 import Search from "./components/search.jsx";
 
-//API
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method : 'GET',
+  headers:{
+    accept: 'application.json',
+    authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect( effect: () => {
+  useEffect(() => {
 
-  }, deps: []);
+  });
 
   return (
     <main>
